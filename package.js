c0777885af34367da5c469d029afa7552965b6aa
An Tran <antran.inbox@gmail.com>
2018-09-09 21:29:53 +0700

    Correct JS after transpiling from CoffeeScript, verify test script

--- package.js
+++ package.js
@@ -1,47 +1,44 @@
 Package.describe({
-  name: 'anttr:restivus',
-  summary: 'Create authenticated REST APIs in Meteor 0.9+ via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
-  version: '0.1.0',
-  git: 'https://github.com/anttr/meteor-restivus.git'
+    name   : 'anttr:restivus',
+    summary: 'Create authenticated REST APIs in Meteor 0.9+ via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
+    version: '0.1.0',
+    git    : 'https://github.com/anttr/meteor-restivus.git',
 });
-
 
 Package.onUse(function (api) {
-  // Minimum Meteor version
-  api.versionsFrom('METEOR@0.9.0');
+    // Minimum Meteor version
+    api.versionsFrom('METEOR@0.9.0');
 
-  // Meteor dependencies
-  api.use('check');
-  api.use('coffeescript');
-  api.use('underscore');
-  api.use('accounts-password@1.3.3');
-  api.use('simple:json-routes@2.1.0');
+    // Meteor dependencies
+    api.use('check');
+    api.use('underscore');
+    api.use('accounts-password@1.3.3');
+    api.use('simple:json-routes@2.1.0');
 
-  api.addFiles('lib/auth.coffee', 'server');
-  api.addFiles('lib/iron-router-error-to-response.js', 'server');
-  api.addFiles('lib/route.coffee', 'server');
-  api.addFiles('lib/restivus.coffee', 'server');
+    api.addFiles('lib/auth.js', 'server');
+    api.addFiles('lib/iron-router-error-to-response.js', 'server');
+    api.addFiles('lib/route.js', 'server');
+    api.addFiles('lib/restivus.js', 'server');
 
-  // Exports
-  api.export('Restivus', 'server');
+    // Exports
+    api.export('Restivus', 'server');
 });
 
-
 Package.onTest(function (api) {
-  // Meteor dependencies
-  api.use('practicalmeteor:munit');
-  api.use('test-helpers');
-  api.use('nimble:restivus');
-  api.use('http');
-  api.use('coffeescript');
-  api.use('underscore');
-  api.use('accounts-base');
-  api.use('accounts-password');
-  api.use('mongo');
+    // Meteor dependencies
+    api.use('practicalmeteor:munit');
+    api.use('test-helpers');
+    api.use('anttr:restivus');
+    api.use('http');
+    api.use('underscore');
+    api.use('accounts-base');
+    api.use('accounts-password');
+    api.use('mongo');
+    api.use('simple:json-routes@2.1.0');
 
-  api.addFiles('lib/route.coffee', 'server');
-  api.addFiles('test/api_tests.coffee', 'server');
-  api.addFiles('test/route_unit_tests.coffee', 'server');
-  api.addFiles('test/authentication_tests.coffee', 'server');
-  api.addFiles('test/user_hook_tests.coffee', 'server');
+    api.addFiles('lib/route.js', 'server');
+    api.addFiles('test/api_tests.js', 'server');
+    api.addFiles('test/route_unit_tests.js', 'server');
+    api.addFiles('test/authentication_tests.js', 'server');
+    api.addFiles('test/user_hook_tests.js', 'server');
 });
